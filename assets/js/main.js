7ad3e0121a7fae8e0a8e1f351ca05f7b84d8182e
Amon <avatchat1@gmail.com>
2026-03-05 20:43:44 +0400

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  if (typeof lucide !== 'undefined' && typeof lucide.createIcons === 'function') {
+    lucide.createIcons();
+  }
+});
+
